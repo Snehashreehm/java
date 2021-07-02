7adb5d5d8b1b4fbda662f9affa63a1e63e9d9693
Snehashreehm <86826588+Snehashreehm@users.noreply.github.com>
2021-07-03 00:06:13 +0530

    Create Addingtwonumber.java

---
+++ Addingtwonumber.java
@@ -0,0 +1,96 @@
+package demo;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class MathTest
+{
+private MathDemo mathDemo;
+ @Before
+public void setUp() throws Exception
+{
+mathDemo = new MathDemo();
+}
+@After
+public void tearDown() throws Exception
+{
+}
+@Test
+public void testAdd() 
+{
+int result = mathDemo.add(5,3);
+assertEquals(result,8);
+ }
+  }
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
