0fcbae9fab4885ec399a41c1c4120a39bdabdbc8
Snehashreehm <86826588+Snehashreehm@users.noreply.github.com>
2021-07-03 01:07:02 +0530

    Create Emp_and_dependants.java

---
+++ Emp_and_dependants.java
@@ -0,0 +1,68 @@
+function getEmployeeFromRec(req) 
+{
+const employee= {
+first_name: req.body.first_name,
+last_name: req.body.last_name,
+email: req.body.email,
+phone_number: req.body.phone_number,   
+hire_date: req.body.hire_date,  
+salary: req.body.salary,
+ manager_id: req.body.manager_id,
+department_id: req.body.department_id   
+};
+return employee;
+}
+async function post(req,res,next) 
+{
+try
+{
+let employee=getEmployeefromeRec(req);
+}
+}
+
+
+    
+
+
+    
+       
+  
+
+
+
+
+
+    
+
+    
+
+
+
+    
+
+
+
+    
+
+
+
+  
+
+
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
